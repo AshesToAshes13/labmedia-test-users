af60f4ba5f718bc272a4599d43014f69afea800f
AshesToAshes13 <egor.bamburov1998@gmail.com>
2023-03-29 18:51:51 +0300

    feat (search-block): сверстал блок поиска

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import './assets/style/styles.css'
 import { getUsers } from "./Api.js";
 
+
+const searchInput = document.getElementById('searchInput')
+const clearButton =document.getElementById('clearSearchButton')
+let selectedFilter = 'date'
 let users = []
 writeUsersInfo()
 async function writeUsersInfo() {
@@ -9,3 +13,17 @@ async function writeUsersInfo() {
     users = usersData
     console.log(users)
 }
+
+searchInput.addEventListener('input',() => {
+    if (searchInput.value !== 0) {
+        clearButton.style.display = 'flex'
+    } else if (searchInput.value === 0 && selectedFilter.length) {
+        clearButton.style.display = 'none'
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    searchInput.value = ''
+    selectedFilter = ''
+    clearButton.style.display = 'none'
+})
